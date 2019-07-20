8d68dace4b6da6af71ac018a4068931a86bb7611
ApplePie <applepie@applepies-mac.local>
2019-07-20 16:28:50 +0300

    Add primary user interface

---
+++ Github-Gists/DetailGistViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailGistViewController.swift
+//  Github-Gists
+//
+//  Created by ApplePie on 20.07.19.
+//  Copyright © 2019 VictorVolnukhin. All rights reserved.
+//
+
+import UIKit
+
+class DetailGistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Github-Gists/GistsViewController.swift
+++ Github-Gists/GistsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GistsViewController.swift
 //  Github-Gists
 //
 //  Created by ApplePie on 20.07.19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GistsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
