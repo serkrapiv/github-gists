2ad278a710363b815647ebc126813d72b20c6353
ApplePie <applepie@applepies-mac.local>
2019-07-20 12:51:27 +0300

    Initial Commit

---
+++ Github-Gists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github-Gists
+//
+//  Created by ApplePie on 20.07.19.
+//  Copyright © 2019 VictorVolnukhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
